7e8a9ca024bc8bb946ada61c894daff3d6f4f5c9
Dima Krasner <dima@dimakrasner.com>
2020-12-09 15:13:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+```
+  __                        _ _  __       
+ / _|_ __ _   _  __ _  __ _| (_)/ _|_   _ 
+| |_| '__| | | |/ _` |/ _` | | | |_| | | |
+|  _| |  | |_| | (_| | (_| | | |  _| |_| |
+|_| |_|   \__,_|\__, |\__,_|_|_|_|  \__, |
+                |___/               |___/
+```
+
+## Background
+
+Traditionally, [Puppy Linux](http://puppylinux.com/) has been based on two kinds of installation:
+1. "Full installation", where the operating system is installed on a bootable partition, like most other distros.
+2. "Frugal installation", where operating system consists of a squashfs image placed on a bootable partition, a writable file system image placed alongside it (the *savefile*) and an initramfs that mounts an [aufs](http://aufs.sourceforge.net/) union file system where the squashfs image is a read-only layer, and changes to the file system are saved in the savefile.
+
+In Puppy jargon, this is translated into a variable called *PUPMODE*. PUPMODE=2 means full installation, and PUPMODE=13 means frugal installation.
+
+This is an over-simplified explanation because PUPMODE is already [explained in detail](https://bkhome.org/archive/puppylinux/development/howpuppyworks.html) by Barry Kauler, the original creator of Puppy and the architect behind its boot process.
+
+Generally, the full installation type is reserved to cases where frugal installation is impossible, because the frugal installation type has many advantages:
+1. It takes less space: the squashfs image containing the operating system is compressed.
+2. It's easier to install, update, inspect and repair: the operating system itself is just the kernel, the initramfs and a squashfs image; the savefile can be deleted to repair or reset the operating system, and backup is a matter of copying the savefile.
+3. It makes it possible to install multiple operating systems (say, different variants of Puppy) on the same partition.
+
+However, inability to perform a frugal installation is not a purely theoretical problem:
+1. Some non-x86 devices have boot loaders that don't support initramfs boot.
+2. Sometimes, one might wish to take the highly modified kernel from a different distro (say, from [Chrome OS](https://www.chromium.org/chromium-os)) in its binary form and use it to boot Puppy, but that kernel is built without initramfs support.
+3. The Puppy initramfs is sensitive to boot device types (e.g. it must contain the required drivers), file system types (e.g. *guess_fstype* needs to be able to detect the file system on the boot partition) and so on; something along the boot process might not work in some configuration where non-initramfs boot would work just fine.
+4. Sometimes, the kernel command-line of kernels borrowed from other operating systems is [hardcoded](https://github.com/archlinuxarm/PKGBUILDs/blob/master/core/linux-veyron/cmdline) in the kernel image.
+5. The use of a savefile is inconvenient: it's slower than saving directly on a partition, it's hard to decide how much space to allocate for the savefile, and limiting the save file size to allow future operating system upgrades is a big compromise on computers with a small 16 GB disk.
+
+## Overview
+
+frugalify is a small, static executable that can be placed on a bootable partition and configured to act as PID 1 via the *init=* kernel parameter.
+
+frugalify simulates what the Puppy initramfs does:
+1. It looks for a squashfs image on the partition mounted by the kernel.
+2. It creates the */save* directory on the partition.
+3. It mounts a union file system.
+4. It starts the Puppy init process under the union file system.
+
+The result is an initramfs-less Puppy installation that combines the advantages of both installation methods:
+1. The operating system is small, because it's compressed.
+2. Updates, repair, etc' are easy, because the operating system is the kernel, the frugalify executable and a squashfs image.
+3. Persistent storage is implemented using a disk partition, and the user does not have to reserve space for it, or think about available free space (i.e. empty the browser cache) all the time.
+4. It's portable: if the kernel can mount the partition, a semi-frugal installation using frugalify is possible.
+
+## Releases
+
+Pre-built, statically-linked and [portable](https://github.com/dimkr/toolchains/) binaries linked against [musl](https://musl.libc.org/) are available in the [releases page](https://github.com/dimkr/frugalify/releases).
+
+## Credits and Legal Information
+
+frugalify is free and unencumbered software released under the terms of the MIT license; see COPYING for the license text.
+
+The ASCII art logo at the top was made using [FIGlet](http://www.figlet.org/).
---
+++ frugalify.c
@@ -0,0 +1,217 @@
+#include <unistd.h>
+#include <linux/loop.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/mount.h>
+
+#ifndef LOOP_CTL_GET_FREE
+#   define LOOP_CTL_GET_FREE 0x4C82
+#endif
+
+#define MAXSFS 8
+
+#ifdef HAVE_AUFS
+#   define FS "aufs"
+#   define FSOPTS_HEAD "br=/save"
+#else
+#   define FS "overlay"
+#   define FSOPTS_HEAD "upperdir=/save,workdir=/.work,lowerdir="
+#endif
+
+static inline void do_autoclose(void *fdp)
+{
+	if (*(int *)fdp != -1)
+		close(*(int *)fdp);
+}
+
+#define autoclose __attribute__((cleanup(do_autoclose)))
+
+// use of sprintf() can double the executable size
+static char *itoa(char *s, int i)
+{
+    if (i >= 10)
+        s = itoa(s, i / 10);
+
+    *s = '0' + i % 10;
+    ++s;
+    *s = '\0';
+    return s;
+}
+
+static char *get_lo_path(const int i)
+{
+    static char loop[sizeof("/save/dev/loop0")] = "/save/dev/loop";
+
+    itoa(loop + sizeof("/save/dev/loop") - 1, i);
+    return loop;
+}
+
+static const char *losetup(const char *sfs, const int i)
+{
+    struct stat stbuf;
+    struct loop_info64 info = {.lo_flags = LO_FLAGS_READ_ONLY};
+    const char *loop;
+    autoclose int loopfd = -1, sfsfd;
+    
+    sfsfd = open(sfs, O_RDWR);
+    if (sfsfd < 0)
+        return NULL;
+
+    if (fstat(sfsfd, &stbuf) < 0)
+        return NULL;
+
+    loop = get_lo_path(i);
+
+    loopfd = open(loop, O_RDWR);
+    if (loopfd < 0)
+        return NULL;
+
+    if (ioctl(loopfd, LOOP_SET_FD, sfsfd) < 0)
+        return NULL;
+
+#ifdef HAVE_STRLCPY
+    strlcpy(info.lo_file_name, sfs, sizeof(info.lo_file_name));
+#else
+    strncpy((char *)info.lo_file_name, sfs, sizeof(info.lo_file_name));
+    info.lo_file_name[sizeof(info.lo_file_name) - 1] = '\0';
+#endif
+    info.lo_sizelimit = (uint64_t)stbuf.st_size;
+    if (ioctl(loopfd, LOOP_SET_STATUS64, &info) < 0)
+        return NULL;
+
+    return loop;
+}
+
+static void losetup_d(const int i)
+{
+    autoclose int loopfd = -1;
+    
+    loopfd = open(get_lo_path(i), O_RDWR);
+    if (loopfd >= 0)
+        ioctl(loopfd, LOOP_CLR_FD, 0);
+}
+
+int main(int argc, char *argv[])
+{
+    static const char *dirs[] = {
+        "/save",
+#ifndef HAVE_AUFS
+        "/.work",
+#endif
+        "/save/.pup_new",
+        "/save/dev",
+    };
+    static char br[1024] = FSOPTS_HEAD;
+    struct dirent ent[MAXSFS];
+    const char *sfs[MAXSFS] = {NULL}, *loop;
+    char sfsmnt[sizeof("/save/.sfs0")] = "/save/.sfs";
+    size_t len, brlen = sizeof(FSOPTS_HEAD) - 1;
+    DIR *root;
+    struct dirent *pent;
+    unsigned int nsfs = 0, i;
+
+    // protect against accidental click
+    if (getpid() != 1)
+        return EXIT_FAILURE;
+
+    root = opendir("/");
+    if (!root)
+        return EXIT_FAILURE;
+    
+    // create a list of all SFS files under /
+    while ((readdir_r(root, &ent[nsfs], &pent) == 0) && (pent != NULL)) {
+        if (pent->d_type != DT_REG)
+            continue;
+
+        len = strlen(pent->d_name);
+        if ((len <= sizeof(".sfs") - 1) || 
+            (memcmp(&pent->d_name[len - 4], ".sfs", sizeof(".sfs") - 1) != 0))
+            continue;
+
+        sfs[nsfs] = pent->d_name;
+        ++nsfs;
+        if (nsfs == MAXSFS)
+            break;
+    }
+    closedir(root);
+
+    if (!sfs[0])
+        return EXIT_FAILURE;
+
+    for (i = 0; i < sizeof(dirs) / sizeof(dirs[0]); ++i) {
+        if ((mkdir(dirs[i], 0755) < 0) && (errno != EEXIST))
+            return EXIT_FAILURE;
+    }
+
+    // mount a devtmpfs so we have the loop%d device nodes
+    if (mount("dev", "/save/dev", "devtmpfs", 0, NULL) < 0)
+        return EXIT_FAILURE;
+    
+    for (i = 0; (i < nsfs) && (brlen < sizeof(br)); ++i) {
+        itoa(sfsmnt + sizeof("/save/.sfs") - 1, i);
+
+        if ((mkdir(sfsmnt, 0755) < 0) && (errno != EEXIST))
+            continue;
+
+        // bind the SFS to a loop device
+        loop = losetup(sfs[i], i);
+        if (!loop) {
+            rmdir(sfsmnt);
+            continue;
+        }
+
+        // mount the loop device
+        if (mount(loop, sfsmnt, "squashfs", 0, NULL) < 0) {
+            losetup_d(i);
+            rmdir(sfsmnt);
+            continue;
+        }
+
+#ifndef HAVE_AUFS
+        if (i == 0)
+            goto cpy;
+#endif
+
+        br[brlen] = ':';
+        ++brlen;
+
+cpy:
+        memcpy(&br[brlen], sfsmnt, sizeof("/save/.sfs0") - 1);
+        brlen += sizeof("/save/.sfs0") - 1;
+    }
+    br[brlen] = '\0';
+    
+    // we no longer need /dev
+    umount2("/save/dev", MNT_DETACH);
+    rmdir("/save/dev");
+
+    // mount a union file system with the SFS mount points and /save on top
+    if (mount(FS, "/save/.pup_new", FS, 0, br) < 0)
+        return EXIT_FAILURE;
+    
+    if (chdir("/save/.pup_new") < 0)
+        return EXIT_FAILURE;
+
+    // make the union file system the file system root
+    if (mount(".", "/", FS, MS_MOVE, NULL) < 0)
+        return EXIT_FAILURE;
+
+    if (chroot(".") < 0)
+        return EXIT_FAILURE;
+
+    if (chdir("/") < 0)
+        return EXIT_FAILURE;
+
+    // run the real init under the new root file system
+    execl("/sbin/init", "init", "initrd_full_install", NULL);
+
+    return EXIT_FAILURE;
+}
